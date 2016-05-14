0ed6c1f3ae5b5339b6a275072ca6319e3f717746
Filip Klimes <filip@filipklimes.cz>
2016-05-14 22:36:13 +0200

    Added support for Attribute.readOnly parameter to widget mapping function

--- src/main/resources/widget/mapping/function.js
+++ src/main/resources/widget/mapping/function.js
@@ -1,6 +1,10 @@
 mappingFunction = function (attribute) {
     var widgetNamespace = "default";
-    if (attribute.primary == true || attribute.model.context == "read" || attribute.model.context == "delete") {
+    if (
+        attribute.primary == true
+        || attribute.model.context == "read"
+        || attribute.model.context == "delete"
+        || (attribute.hasOwnProperty("readOnly") && attribute.readOnly == true)) {
         widgetNamespace = "disabled";
     }
 
